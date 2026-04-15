b38e6ea9cca687011dc6dbc0ce3ea88176c7e7a1
Sathish P <sathishp100sms@gmail.com>
2026-04-15 18:53:45 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],      
+    allow_origins=["https://speakcoach.netlify.app"],      
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
